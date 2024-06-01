a379bc0b88ca29c12b85725c3b2d12b58fc343c5
lmeadows <meadows.lucas@gmail.com>
2024-05-31 22:25:26 -0600

    Getting closer, but the validation logic is returning incorrect in some cases, such as returning true when ticket numbers are missing and returning false for merges and reverts

--- validate_commits.sh
+++ validate_commits.sh
@@ -1,3 +1,14 @@
+valid_commit_prefix () {
+  MESSAGE=$1
+  REGEX="^(PODA|PODB|PODC|MGMT)-(\d+)*"
+  if [[ $MESSAGE =~ $REGEX ]]
+  then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
 RESPONSE=$(curl -sL \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
@@ -9,5 +20,13 @@ LINES=$(echo $RESPONSE | sed 's/\\n/ /g' | jq -r ".[] | .commit.message, .author
 while read -r MESSAGE; do
   read -r AUTHOR
   read -r SHA
-  echo "AUTHOR: $AUTHOR, SHA: $SHA"
+  IS_VALID=$(valid_commit_prefix "$MESSAGE")
+  if [[ $IS_VALID == 1 ]]
+  then
+    IS_VALID="TRUE"
+  else
+    IS_VALID="FALSE"
+  fi
+  echo "$SHA $IS_VALID $AUTHOR"
+
 done <<< "$LINES"
